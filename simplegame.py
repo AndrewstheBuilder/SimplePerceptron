f8ca5b9c72c509046da4a27d894b1a0ec739e3ab
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2023-09-23 22:05:49 -0400

    Add game of dot generation. There are two classes of dots (black and white) and they are linearly separably. TODO: Will use a linear network to draw a line to separate the two classes of dots as they are being drawn.

--- simplegame.py
+++ simplegame.py
@@ -1,23 +1,58 @@
 import pygame
 from pygame.locals import *
 import sys
+import random
 
 pygame.init()
 
-# Predefined some colors
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
-GREEN = (0, 255, 0)
+# Constants
+WIDTH, HEIGHT = 600, 600
+DOT_SIZE = 4
+DELAY = 10
+
+# Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-DISPLAYSURF = pygame.display.set_mode((600,600))
+# Initialize display
+DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Simple Game")
 
+# Randomly generate line's slope and intercept
+m = random.uniform(-1, 1)
+b = random.uniform(0, HEIGHT)
+
+def is_above_line(x, y):
+    """Check if point (x, y) is above the line."""
+    return y < m * x + b
+
+def draw_dot(x, y, is_above):
+    """Draw a dot on the display based on its class."""
+    if is_above:
+        pygame.draw.circle(DISPLAYSURF, BLACK, (x, y), DOT_SIZE, 1) # Black outline
+        pygame.draw.circle(DISPLAYSURF, WHITE, (x, y), DOT_SIZE - 1)    # White inside
+    else:
+        pygame.draw.circle(DISPLAYSURF, BLACK, (x, y), DOT_SIZE)
+
+# Draw dots
+for _ in range(2000):
+    x, y = random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1)
+
+    draw_dot(x, y, is_above_line(x, y))
+
+    pygame.display.update()
+    pygame.time.delay(DELAY)
+
+    # Allow closing the window during drawing
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+# Main game loop
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.update()
