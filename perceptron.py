691ec806c6806854171ccf504f9db308723b4a3e
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2023-09-25 09:37:49 -0400

    Create Perceptron.py, .gitignore. Do the neural net stuff

---
+++ perceptron.py
@@ -0,0 +1,19 @@
+import random
+
+class Perceptron:
+    def __init__(self, n):
+        self.weights = [random.uniform(-1, 1) for _ in range(n)]
+        self.c = 0.01
+
+    def feedforward(self, inputs):
+        total = sum(input_val * weight for input_val, weight in zip(inputs, self.weights))
+        return self.activate(total)
+
+    def activate(self, total):
+        return 1 if total > 0 else -1
+
+    def train(self, inputs, desired):
+        guess = self.feedforward(inputs)
+        error = desired - guess
+        for i, input_val in enumerate(inputs):
+            self.weights[i] += self.c * error * input_val
--- simplegame.py
+++ simplegame.py
@@ -2,8 +2,12 @@ import pygame
 from pygame.locals import *
 import sys
 import random
+from perceptron import Perceptron
 
 pygame.init()
+
+# define a Perceptron
+ptron = Perceptron(3)
 
 # Constants
 WIDTH, HEIGHT = 600, 600
@@ -13,6 +17,8 @@ DELAY = 10
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 
 # Initialize display
 DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -21,15 +27,27 @@ pygame.display.set_caption("Simple Game")
 
 # Randomly generate line's slope and intercept
 m = random.uniform(-1, 1)
-b = random.uniform(0, HEIGHT)
+b = random.uniform(HEIGHT//4, HEIGHT//2)
 
 def is_above_line(x, y):
-    """Check if point (x, y) is above the line."""
-    return y < m * x + b
+    """
+    Check if point (x, y) is above the line y = m*x + b.
+
+    Note: In computer graphics (like Pygame), the origin (0, 0) starts at the top-left
+    corner of the screen. As the y-value increases, you move downwards on the screen.
+    Therefore, if the given y is less than the calculated y for the line, the point
+    is considered to be "above" the line in this coordinate system.
+    """
+    return 1 if y < m * x + b else 0
+
+def draw_guess(x, y, guess):
+    """Draw a dot on the display based on the perceptron's guess."""
+    color = BLUE if guess == 1 else RED
+    pygame.draw.circle(DISPLAYSURF, color, (x, y), DOT_SIZE)
 
 def draw_dot(x, y, is_above):
     """Draw a dot on the display based on its class."""
-    if is_above:
+    if is_above == 1:
         pygame.draw.circle(DISPLAYSURF, BLACK, (x, y), DOT_SIZE, 1) # Black outline
         pygame.draw.circle(DISPLAYSURF, WHITE, (x, y), DOT_SIZE - 1)    # White inside
     else:
@@ -38,8 +56,13 @@ def draw_dot(x, y, is_above):
 # Draw dots
 for _ in range(2000):
     x, y = random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1)
+    inputs = [x,y,1]
+    answer = is_above_line(x, y)
+    ptron.train(inputs, answer)
+    draw_dot(x, y, answer)
 
-    draw_dot(x, y, is_above_line(x, y))
+    guess = ptron.feedforward(inputs)
+    draw_guess(x,y,guess)
 
     pygame.display.update()
     pygame.time.delay(DELAY)
