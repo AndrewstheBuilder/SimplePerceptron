6db45203beb9f9a950bafd6357ea14ba6ffdf8ce
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2023-09-25 10:04:00 -0400

    Autoformat files

--- perceptron.py
+++ perceptron.py
@@ -1,12 +1,14 @@
 import random
 
+
 class Perceptron:
     def __init__(self, n):
         self.weights = [random.uniform(-1, 1) for _ in range(n)]
         self.c = 0.01
 
     def feedforward(self, inputs):
-        total = sum(input_val * weight for input_val, weight in zip(inputs, self.weights))
+        total = sum(input_val * weight for input_val,
+                    weight in zip(inputs, self.weights))
         return self.activate(total)
 
     def activate(self, total):
--- simplegame.py
+++ simplegame.py
@@ -29,6 +29,7 @@ pygame.display.set_caption("Simple Game")
 m = random.uniform(-1, 1)
 b = random.uniform(HEIGHT//4, HEIGHT//2)
 
+
 def is_above_line(x, y):
     """
     Check if point (x, y) is above the line y = m*x + b.
@@ -40,29 +41,34 @@ def is_above_line(x, y):
     """
     return 1 if y < m * x + b else 0
 
+
 def draw_guess(x, y, guess):
     """Draw a dot on the display based on the perceptron's guess."""
     color = BLUE if guess == 1 else RED
     pygame.draw.circle(DISPLAYSURF, color, (x, y), 1)
 
+
 def draw_dot(x, y, is_above):
     """Draw a dot on the display based on its class."""
     if is_above == 1:
-        pygame.draw.circle(DISPLAYSURF, BLACK, (x, y), DOT_SIZE, 1) # Black outline
-        pygame.draw.circle(DISPLAYSURF, WHITE, (x, y), DOT_SIZE - 1)    # White inside
+        pygame.draw.circle(DISPLAYSURF, BLACK, (x, y),
+                           DOT_SIZE, 1)  # Black outline
+        pygame.draw.circle(DISPLAYSURF, WHITE, (x, y),
+                           DOT_SIZE - 1)    # White inside
     else:
         pygame.draw.circle(DISPLAYSURF, BLACK, (x, y), DOT_SIZE)
+
 
 # Draw dots
 for _ in range(2000):
     x, y = random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1)
-    inputs = [x,y,1]
+    inputs = [x, y, 1]
     answer = is_above_line(x, y)
     ptron.train(inputs, answer)
     draw_dot(x, y, answer)
 
     guess = ptron.feedforward(inputs)
-    draw_guess(x,y,guess)
+    draw_guess(x, y, guess)
 
     pygame.display.update()
     pygame.time.delay(DELAY)
